0f211cdf676c4b36c8d1422005b3e4e8ef17dd0c
Haseeb <haseeb@example.com>
2026-08-11 12:01:13 +0500

    Update android build configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.my_khata"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
